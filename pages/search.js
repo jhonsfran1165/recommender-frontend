cee89e7274b24df74d762cecfe26f111182ae45e
Sebastian Franco <jsfranco@whaleandjaguar.co>
2022-05-15 14:21:09 -0500

    chore: local meiliserach instance

--- pages/search.js
+++ pages/search.js
@@ -1,28 +1,46 @@
-
+import {
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  IconButton,
+  Spacer,
+  Stack,
+  Text,
+  useColorMode,
+  VStack,
+} from "@chakra-ui/react";
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
-
-import { RefinementList, Configure, ClearRefinements, Hits, InstantSearch, SearchBox, SortBy, Pagination } from "react-instantsearch-dom";
-import { Stack, VStack, HStack, Flex, Heading, Box } from '@chakra-ui/react'
-import { FaSearchengin, FaMoon, FaSun } from "react-icons/fa";
-import { Grid, GridItem, Text, IconButton, Spacer, useColorMode } from '@chakra-ui/react'
-
+import { FaMoon, FaSearchengin, FaSun } from "react-icons/fa";
+import {
+  ClearRefinements,
+  Configure,
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+  SortBy,
+} from "react-instantsearch-dom";
 
 const searchClient = instantMeiliSearch(
-  "https://integration-demos.meilisearch.com",
-  "q7QHwGiX841a509c8b05ef29e55f2d94c02c00635f729ccf097a734cbdf7961530f47c47"
+  "http://localhost:7700",
+  "asd7687asdasdkjhwrdf97fcsadfs"
 );
-
 
 export default function SearchEngine(props) {
   const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark"
+  const isDark = colorMode === "dark";
 
   return (
     <VStack p={5}>
       <Flex w="100%">
         <HStack>
-          <Text fontSize='3xl' color="red.400"><FaSearchengin /></Text>
-          <Heading pl={2} fontSize='3xl' ml="2" color="red.400">
+          <Text fontSize="3xl" color="red.400">
+            <FaSearchengin />
+          </Text>
+          <Heading pl={2} fontSize="3xl" ml="2" color="red.400">
             Booker Univalle
           </Heading>
         </HStack>
@@ -39,62 +57,61 @@ export default function SearchEngine(props) {
           <Heading
             lineHeight={1.1}
             fontWeight={400}
-            fontSize={{ base: 'xl', sm: '2xl', lg: '3xl' }}
+            fontSize={{ base: "xl", sm: "2xl", lg: "3xl" }}
           >
             <Text
-              as={'span'}
-              position={'relative'}
+              as={"span"}
+              position={"relative"}
               _after={{
                 content: "''",
-                width: 'full',
-                height: '30%',
-                position: 'absolute',
+                width: "full",
+                height: "30%",
+                position: "absolute",
                 bottom: 1,
                 left: 0,
-                bg: 'red.400',
+                bg: "red.400",
                 zIndex: -1,
-              }}>
+              }}
+            >
               Busca tus libros,
             </Text>
             <br />
-            <Text as={'span'} color={'red.400'}>
+            <Text as={"span"} color={"red.400"}>
               obtén recomendaciones.
             </Text>
             <br />
-            <Text as={'span'}>
-              Nunca fue tan fácil estudiar.
-            </Text>
-            </Heading>
+            <Text as={"span"}>Nunca fue tan fácil estudiar.</Text>
+          </Heading>
         </Flex>
       </Stack>
       <Stack p={10} w="100%">
         <Flex w="100%">
-          <InstantSearch indexName="steam-video-games" searchClient={searchClient}>
+          <InstantSearch indexName="copies" searchClient={searchClient}>
             <Grid
               templateRows="repeat(2, 1fr)"
               templateColumns="repeat(6, 1fr)"
               gap={5}
             >
-              <GridItem rowSpan={2} w='100%' colSpan={1}>
+              <GridItem rowSpan={2} w="100%" colSpan={1}>
                 <ClearRefinements />
                 <SortBy
-                  defaultRefinement="steam-video-games"
+                  defaultRefinement="copies"
                   items={[
-                    { value: "steam-video-games", label: "Relevant" },
+                    { value: "copies", label: "BarCode" },
                     {
-                      value: "steam-video-games:recommendationCount:desc",
+                      value: "copies:bar_code:desc",
                       label: "Most Recommended",
                     },
                     {
-                      value: "steam-video-games:recommendationCount:asc",
+                      value: "copies:bar_code:asc",
                       label: "Least Recommended",
                     },
                   ]}
                 />
                 <h2>Biblioteca</h2>
-                <RefinementList attribute="genres" />
+                <RefinementList attribute="medium_type" />
                 <h2>Tipo</h2>
-                <RefinementList attribute="misc" />
+                <RefinementList attribute="location" />
                 <Configure
                   hitsPerPage={5}
                   attributesToSnippet={["description:50"]}
@@ -102,7 +119,7 @@ export default function SearchEngine(props) {
                 />
                 Menú buscador
               </GridItem>
-              <GridItem colSpan={4} w='100%'>
+              <GridItem colSpan={4} w="100%">
                 <SearchBox />
                 <Hits />
               </GridItem>
@@ -111,8 +128,8 @@ export default function SearchEngine(props) {
               </GridItem>
             </Grid>
           </InstantSearch>
-      </ Flex>
-    </Stack>
+        </Flex>
+      </Stack>
     </VStack>
   );
 }
